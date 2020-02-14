d9a1ade034bd8d25d1568265290eea1d04524d67
kellyjeffery <kellyjeffery1@hotmail.com>
2020-02-14 11:00:16 +0000

    devise updated'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :books
+  has_many :pages
   validates :name, uniqueness: true
   validates :password, uniqueness: true
   validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
